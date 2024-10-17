4294a68624ef9dc836666761e914d8cba3c1273f
jicodes <122512755+jicodes@users.noreply.github.com>
2024-10-16 20:30:34 -0700

    add solution to leetcode 735 - Stack

---
+++ 06-stack/735_asteroid_collision.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            # Process positive and negative asteroids
+            while stack and asteroid < 0 < stack[-1]:  # Collision condition
+                if stack[-1] < -asteroid:  # Top of the stack is smaller
+                    stack.pop()  # Destroy the top asteroid
+                    continue
+                elif stack[-1] == -asteroid:  # Equal size
+                    stack.pop()  # Both destroy each other
+                break
+            else:
+                stack.append(asteroid)  # Add the asteroid if no collision
+
+        return stack
